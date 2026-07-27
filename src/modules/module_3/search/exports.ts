c2f5577e6f170256c310949db43cd0488cc83113
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-07-26 21:37:58 -0400

    :recycle: refactor(module): modularize module structure and remove external dependencies

--- src/modules/module_3/search/exports.ts
+++ src/modules/module_3/search/exports.ts
@@ -1,3 +1,2 @@
 export { searchPosts } from '@module_3/search/actions/search';
 export { default as SearchInput } from '@module_3/search/components/SearchInput';
-export { default as SearchBox } from '@module_3/search/components/SearchBox';
