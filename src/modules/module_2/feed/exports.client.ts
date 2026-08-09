83240876bc89ba116c6edcbb5eeafa6baf074b0a
Neritoou <agdsds245@gmail.com>
2026-08-09 19:57:52 -0400

    :sparkles: feat(communities): add feed barrel exports

---
+++ src/modules/module_2/feed/exports.client.ts
@@ -0,0 +1,5 @@
+export { FeedContainer } from './components/feed-container'
+export { CommunityFeedSection } from './components/community-feed-section'
+export { HomeFeedSection } from './components/home-feed-section'
+export { UserFeedSection } from './components/user-feed-section'
+export type { FeedPost, PaginatedFeed, FeedFilter } from './types'
